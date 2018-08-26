4b8f3664022e9bb62a54059e966056e1c621f31c
vihangaw <vyasith@gmail.com>
2018-08-26 22:14:59 +0530

    # App validation firebsae

--- app/src/main/java/com/paidtocode/stargang/ui/SplashActivity.java
+++ app/src/main/java/com/paidtocode/stargang/ui/SplashActivity.java
@@ -6,12 +6,19 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.paidtocode.stargang.R;
 import com.paidtocode.stargang.StarGangApplication;
 import com.paidtocode.stargang.api.APIHelper;
@@ -26,6 +33,8 @@ import com.paidtocode.stargang.util.UserSessionManager;
 import org.json.JSONException;
 
 public class SplashActivity extends AppCompatActivity {
+
+	private static final String TAG = SplashActivity.class.getSimpleName();
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -44,14 +53,14 @@ public class SplashActivity extends AppCompatActivity {
 //			}
 		}
 		if (UserSessionManager.getInstance().isLogin()) {
-			if (checkNetwork())
+			if (checkNetwork()) {
 				new Handler().postDelayed(new Runnable() {
 					@Override
 					public void run() {
 						fetchUser();
 					}
-				}, 950);
-			else {
+				}, 50);
+			} else {
 				new Handler().postDelayed(new Runnable() {
 					@Override
 					public void run() {
@@ -72,29 +81,47 @@ public class SplashActivity extends AppCompatActivity {
 	}
 
 	private void fetchUser() {
-		new UserRequestHelperImpl().getUserProfile(new APIHelper.PostManResponseListener() {
+		DatabaseReference myRef = FirebaseDatabase.getInstance().getReference("allow");
+		myRef.addListenerForSingleValueEvent(new ValueEventListener() {
 			@Override
-			public void onResponse(Ancestor ancestor) {
-				if (ancestor.getStatus() && ancestor instanceof SignUpResponse) {
-					Signup data = ((SignUpResponse) ancestor).getData();
-					if (data != null) {
-						proceedLogin(data);
-					}
+			public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+				// This method is called once with the initial value and again
+				// whenever data at this location is updated.
+				String value = dataSnapshot.getValue(String.class);
+				if (!TextUtils.isEmpty(value) && value.equalsIgnoreCase("1")) {
+					new UserRequestHelperImpl().getUserProfile(new APIHelper.PostManResponseListener() {
+						@Override
+						public void onResponse(Ancestor ancestor) {
+							if (ancestor.getStatus() && ancestor instanceof SignUpResponse) {
+								Signup data = ((SignUpResponse) ancestor).getData();
+								if (data != null) {
+									proceedLogin(data);
+								}
+							}
+						}
+
+						@Override
+						public void onError(Error error) {
+							if (error != null) {
+								if (!TextUtils.isEmpty(error.getMessage())) {
+									Toast.makeText(SplashActivity.this,
+											error.getMessage(), Toast.LENGTH_SHORT).show();
+								}
+							}
+							startActivity(new Intent(SplashActivity.this, DashbordActivity.class));
+							finish();
+						}
+					});
 				}
 			}
 
 			@Override
-			public void onError(Error error) {
-				if (error != null) {
-					if (!TextUtils.isEmpty(error.getMessage())) {
-						Toast.makeText(SplashActivity.this,
-								error.getMessage(), Toast.LENGTH_SHORT).show();
-					}
-				}
-				startActivity(new Intent(SplashActivity.this, DashbordActivity.class));
-				finish();
+			public void onCancelled(DatabaseError error) {
+				// Failed to read value
+				Log.w(TAG, "Failed to read value.", error.toException());
 			}
 		});
+
 	}
 
 	private void proceedLogin(Signup signup) {
